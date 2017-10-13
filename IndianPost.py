c6485b0a204853bf4553c8ec9d5cf84b8e978b80
Rajesh Kumar <30926420+rkay26@users.noreply.github.com>
2017-10-13 21:20:48 +0530

    Update IndianPost.py
    After decode the captcha , extracting the data from the Indian Post website and print the data or output in Json format.

--- IndianPost.py
+++ IndianPost.py
@@ -3,6 +3,19 @@ import pytesseract
 import os
 import urllib.request
 
+# Print in JSON format
+def jsonFormat(tracking_id, booked_on, status, delivered_at):
+    print(json.dumps(
+            {
+                    'tracking_id': tracking_id, 
+                    'ship_date': booked_on,
+                    'status':status,
+                    'delivery date':delivered_at,
+            }, sort_keys=True, indent=4)
+    )
+    pass
+
+# Clean the captch
 def cleanImage(imagePath):
     image = Image.open(imagePath)
     image = image.point(lambda x: 0 if x<143 else 255)
@@ -10,6 +23,7 @@ def cleanImage(imagePath):
     borderImage.save(imagePath)
     pass
 
+# Get text from the Captcha
 def getCaptha(captchaUrl):
     urllib.request.urlretrieve(captchaUrl, "captcha.gif")
     captcha_path = os.getcwd() + "\\captcha.gif"
@@ -19,19 +33,61 @@ def getCaptha(captchaUrl):
     return text
 
 if __name__ == '__main__':
-    #tracking_id = 'RM719962415IN EP470107781IN'
-    tracking_id = input()
+#    tracking_id = 'RM719962415IN' 
+    tracking_id = 'EP470107781IN'
+#    tracking_id = input()
+    
+    # URL of the website
     url ='https://www.indiapost.gov.in/VAS/Pages/TrackConsignment.aspx'
-    response = requests.get(url)
-    soup = bs(response.content, 'html.parser')
     
-    pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+    retreived = True
+    # Will run till successful log-in is made
+    while retreived:
+        
+        # Create a session
+        session = requests.Session()
+        
+        # Get data from the URL
+        html = session.get(url).content
+        
+        # Parse the data received
+        soup = bs(html, 'html.parser')
+        
+        # ID of text boxes on the web page
+        consignment_number_id = 'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_txtOrignlPgTranNo'
+        consignment_box_id = 'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_txtCaptcha'
+        
+        # Extract data of image tag to get URL of captcha
+        captcha_image = soup.find('img', attrs={'id':'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_imgCaptcha'})
+        captcha_src = url[:33] + captcha_image['src'][3:]
+        
+        pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+        captcha_text = getCaptcha(captcha_src)
+        
+        print(captcha_text)
     
-    consignment_number = soup.find('input', attrs={'id':'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_txtOrignlPgTranNo'})
-    captcha_image = soup.find('img', attrs={'id':'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_imgCaptcha'})
-    captcha_box = soup.find('input', attrs={'id':'ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_txtCaptcha'})
-    captch_src = 'https://www.indiapost.gov.in/VAS/' + captcha_image['src'][3:]
-    print(captch_src)
-    captch_text = getCaptha(captch_src)
-    print(captch_text)
-    
+        # Values for the form
+        credentials = {
+            consignment_number_id : tracking_id,
+            consignment_box_id : captcha_text,
+        }
+        
+        # Submit form
+        session.post(url, data=credentials)
+        
+        # Get data after successful login
+        response = session.get(url).content
+        soup2 = bs(response, 'html.parser')
+        
+        # Extract values related to consignment
+        booked_on = soup2.find('td', attrs={'data-th':'Booked OnTime'})
+        status = soup2.find('span', attrs={'class':'bold_txt TrackConsignIntstatus'})
+        delivered_at = soup2.find('td', attrs={'data-th':'Delivered At'})
+        
+        print(booked_on)
+        
+        # If captcha matches the captcha text then print information in JSON format
+        if booked_on is not None:
+            jsonFormat(tracking_id, booked_on.text, status.text[18:], delivered_at.text)
+            break
+        
